41e8924e22552a990762adce64af89fc2201a0b2
Ali Koca <kinetixcicocuk@gmail.com>
2022-05-25 20:25:17 +0300

    CMakeLists.txt: CXX -> C, wrong flag

--- sh.c
+++ sh.c
@@ -9,6 +9,21 @@
 #include "include/std.h"
 #include "include/linenoise.h"
 
+char *hints(const char *buff, int *color, int *bold){
+	if (!strcmp(buff, "l")){
+		*color = 2;
+		*bold = 0;
+		return "s";
+	}
+	return NULL;
+}
+
+void completion(const char *buff, linenoiseCompletions *lc){
+	if (!strcmp(&buff[0], "l")){
+		linenoiseAddCompletion(lc, "ls");
+	}
+}
+
 int spawn_oshean(){
 	// Variables needed by program
 	size_t size = 0;
@@ -31,23 +46,31 @@ int spawn_oshean(){
 		exit(1);
 	}
 
+	// Prompt formatting
 	if (sprintf(prompt, "<\033[0;34m%s@%s\033[0;37m> ", user, hostname)  < 0){
 		printf("Exiting due to sprintf\n");
 		exit(1);
 	}
 
+	linenoiseSetHintsCallback(hints);
+	linenoiseSetCompletionCallback(completion);
+
+	// Prompt input
 	while((input_cmd_oshean_bf_tr = linenoise(prompt)) != NULL){
+		// Trim the string and return address
 		char *input_cmd_oshean = osh_trim(input_cmd_oshean_bf_tr);
-		
+	
+		linenoiseHistoryAdd(input_cmd_oshean);
+
+		// Space check	
 		if (!strcmp(input_cmd_oshean, ""))
 			continue;
 
-		// shell username and hostname print and get input with fgets in down,
 		// Check errors and execute command
 		if((n = cmd_exec_oshean(input_cmd_oshean)) != 0){
 			printf("RET: %d\n", n);
 		}
 
-		linenoiseFree(input_cmd_oshean_bf_tr);
+		free(input_cmd_oshean_bf_tr);
 	}
 }
